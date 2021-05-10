0861b996a5db9e31ec50878bff91b75ce1d308e6
Javier Garcia Cotado <javier.garcia_cotado@roche.com>
2021-05-10 12:19:17 +0200

    Adds e2e tests

---
+++ test/e2e/integration/task.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+context('Actions', () => {
+  it('creates task', () => {
+    cy.intercept('GET', '**/task/*').as('getTask');
+    cy.visit('http://localhost:8030/task/add');
+    cy.get('[name="title"]').type('task 1');
+    cy.get('[name="content"]').type('do whatever');
+    cy.get('[type="submit"]').click();
+    cy.wait('@getTask')
+      .its('response.statusCode')
+      .should('be.oneOf', [200]);
+    cy.get('.task')
+      .last()
+      .find('.title h4')
+      .should('have.text', 'task 1');
+    cy.get('.task')
+      .last()
+      .find('.content span')
+      .should('have.text', 'do whatever');
+  });
+});
